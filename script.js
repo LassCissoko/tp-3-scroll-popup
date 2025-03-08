68c5d1d1ddbde6b384d9cc6f1b7419c9dd99103d
LassCissoko <lassanadu910@gmail.com>
2025-03-08 17:09:22 +0100

    TP complété

--- script.js
+++ script.js
@@ -7,3 +7,37 @@
 // Faire apparaitre la popup quand on est en bas du site
 
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
+
+const nav            = document.querySelector('#navbar');
+const imgLeft        = document.querySelector('#imgImprovise')
+const popUp          = document.querySelector('#popup');
+const closeBtn       = document.querySelector('#closeBtn');
+
+window.addEventListener('scroll', () => {
+  // Événement NavBar
+  if (window.scrollY > 1) {
+    nav.style.height=  50 + "px";
+  } else {
+    nav.style.height=  90 + "px";
+  }
+// Événement imgImprovise 
+  if (window.scrollY >= 244) {
+    imgLeft.style.opacity = 1
+    imgLeft.style.transform = "translateX(0px)";
+  }
+
+// Si l'hauteur actuel + l'hauteur de la fênetre === hauteur de la page éxecute la condition
+  if (window.scrollY + window.innerHeight === document.body.scrollHeight ) {
+    popUp.style.opacity = 1;
+    popUp.style.transform = "translateX(0)";
+  }
+});
+
+// Événement btn
+closeBtn.addEventListener('click', () => {
+  popUp.style.transform = ('translateX(800px)')
+
+setTimeout(() => {
+  popUp.remove()
+}, 2000);  
+});
