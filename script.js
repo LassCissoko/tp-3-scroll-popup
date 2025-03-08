d509cf57860119298a46407692cb0e57de2fd1bd
LassCissoko <lassanadu910@gmail.com>
2025-03-08 17:56:21 +0100

    Correction TP

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 // Créer un événement au scroll
-
 // Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
-
 // Faire apparaitre l'image de la partie improvise
-
 // Faire apparaitre la popup quand on est en bas du site
-
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
 
 const nav            = document.querySelector('#navbar');
 const imgLeft        = document.querySelector('#imgImprovise')
 const popUp          = document.querySelector('#popup');
 const closeBtn       = document.querySelector('#closeBtn');
+let playOnce         = true;
 
 window.addEventListener('scroll', () => {
   // Événement NavBar
@@ -20,24 +17,43 @@ window.addEventListener('scroll', () => {
   } else {
     nav.style.height=  90 + "px";
   }
-// Événement imgImprovise 
-  if (window.scrollY >= 244) {
+  // Événement imgImprovise 
+
+  let scrollValue = (window.scrollY + window.innerHeight) / document.body.scrollHeight;
+  if (scrollValue > 0.45) {
     imgLeft.style.opacity = 1
-    imgLeft.style.transform = "translateX(0px)";
+    imgLeft.style.transform = "none";
   }
 
-// Si l'hauteur actuel + l'hauteur de la fênetre === hauteur de la page éxecute la condition
-  if (window.scrollY + window.innerHeight === document.body.scrollHeight ) {
+  // Méthode FS
+
+  if (scrollValue === 1 && playOnce) {
     popUp.style.opacity = 1;
-    popUp.style.transform = "translateX(0)";
+    popUp.style.transform = "none";
+    playOnce = false;
   }
-});
+
+  closeBtn.addEventListener('click', () => {
+    popUp.style.transform = 'translateX(800px)';
+  });
+
+
+
+  // Méthode Lass
+// Si l'hauteur actuel + l'hauteur de la fênetre === hauteur de la page éxecute la condition
+//   if (scrollValue === 1 ) {
+//     popUp.style.opacity = 1;
+//     popUp.style.transform = "none";
+//   }
+// });
 
 // Événement btn
-closeBtn.addEventListener('click', () => {
-  popUp.style.transform = ('translateX(800px)')
 
-setTimeout(() => {
-  popUp.remove()
-}, 2000);  
+// closeBtn.addEventListener('click', () => {
+//   popUp.style.transform = 'translateX(800px)';
+
+// setTimeout(() => {
+//   popUp.remove()
+// }, 500);  
+// });
 });
